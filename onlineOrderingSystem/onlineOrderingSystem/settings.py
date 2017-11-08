e190ea6c5091717e7455c97708754148d8ddf8fa
Joseph Mckulka <jmckulk@Josephs-MacBook-Pro.local>
2017-11-08 16:03:17 -0500

    link database and django

--- onlineOrderingSystem/onlineOrderingSystem/settings.py
+++ onlineOrderingSystem/onlineOrderingSystem/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'onlineOrderingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'F17OnlineOrdering',
+        'USER': 'uwe5tglx',
+        'PASSWORD': 'cpsc4910',
+        'HOST': 'mysql1.cs.clemson.edu',
     }
 }
 
