6dc650dfff4fa68c8b83dd73a452bc1dd008951d
Nathan Harrell <nharrel@clemson.edu>
2017-11-21 01:30:45 -0500

    Finished functioning ticket view page and created base template.

--- onlineOrderingSystem/tickets/views.py
+++ onlineOrderingSystem/tickets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Ticket, Side, Meal
 
+
 # Create your views here.
 def kitchen_view(request):
     sixOldestTickets = Ticket.objects.order_by('creation_time_and_date')
@@ -8,7 +9,10 @@ def kitchen_view(request):
     return render(request, 'tickets/view_orders.html', {
         'displayTickets': sixOldestTickets,
     })
-	
+
+
 def manager_view(request):
-	all_tickets = Ticket.objects.order_by('creation_time_and_date')
-	return render(request, 'tickets/tables.html', {'alltickets':all_tickets})
+    all_tickets = Ticket.objects.order_by('creation_time_and_date')
+    return render(request, 'tickets/tickets.html', {
+        'alltickets': all_tickets
+    })
