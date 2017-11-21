860162852f8926f5b754a9eb049b69c71be0585b
Nathan Harrell <nharrel@clemson.edu>
2017-11-21 00:35:20 -0500

    Added static files (CSS, etc) to Django.

--- onlineOrderingSystem/tickets/views.py
+++ onlineOrderingSystem/tickets/views.py
@@ -3,7 +3,7 @@ from .models import Ticket, Side, Meal
 
 # Create your views here.
 def kitchen_view(request):
-    sixOldestTickets = ticket.objects.order_by('creation_time_and_date')
+    sixOldestTickets = Ticket.objects.order_by('creation_time_and_date')
 
     return render(request, 'tickets/view_orders.html', {
         'displayTickets': sixOldestTickets,
