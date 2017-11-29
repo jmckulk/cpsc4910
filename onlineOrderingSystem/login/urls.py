528385a5fa95a99fbdddbd5ff45439d6b19d1f55
Nathan Harrell <nharrel@clemson.edu>
2017-11-28 23:52:45 -0500

    Modifed login to use custom HTML instead of built in Django HTML

--- onlineOrderingSystem/login/urls.py
+++ onlineOrderingSystem/login/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
 
 app_name = 'login'
 
 urlpatterns = [
-    #url(r'^login/$', views.login, name="login"),
-	url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', views.userLogin, name="login"),
+	url(r'^logout/$', views.userLogout, name='logout'),
 ]
--- onlineOrderingSystem/login/views.py
+++ onlineOrderingSystem/login/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from .models import Account
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
-def login(request):
-    log_in = Account.objects
-    return render(request, 'login/login.html', {
-        'login': log_in
-    })
-	
-def checkLogin(request):
-	if not request.user.is_authenticated():
-		if request.method == 'POST':
-			username = request.POST.get("username", "")
-			password = request.POST.get("password", "")
-			user = authenticate(username=username, password=password)
-			
-			if user is not None:
-				login(request, user)
+
+def userLogin(request):
+    if not request.user.is_authenticated():
+        if request.method == 'GET':
+            return render(request, 'login/login.html', {})
+
+        elif request.method == 'POST':
+            username = request.POST.get("usernameInput", "")
+            password = request.POST.get("passwordInput", "")
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HttpResponse("<h1>Login.</h1>")
+            else:
+                return render(request, 'login/login.html', {
+                    'error': True,
+                })
+
+    else:
+        return HttpResponse("<h1>Already logged in.</h1>")
+
+
+def userLogout(request):
+    logout(request)
+    return HttpResponse("<h1>logout.</h1>")
