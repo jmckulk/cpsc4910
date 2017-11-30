1c383d80662567a9f2b8aa412a11d33272e2327e
Nathan Harrell <nharrel@clemson.edu>
2017-11-29 21:03:35 -0500

    Added ability to add items to menu

--- onlineOrderingSystem/menu/urls.py
+++ onlineOrderingSystem/menu/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'menu'
 
 urlpatterns = [
-    # api access points for dealing with questionnaire data
     url(r'^menu/$', views.menu, name="menu"),
+    url(r'^create_meal/$', views.create_meal, name="create_meal"),
+    url(r'create_side/$', views.create_side, name="create_side"),
+    url(r'create_drink/$', views.create_drink, name="create_drink"),
 ]
--- onlineOrderingSystem/menu/views.py
+++ onlineOrderingSystem/menu/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Meal, Side, Drink
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-# Create your views here.
 
 
 def menu(request):
@@ -15,6 +14,91 @@ def menu(request):
             'allsideitems': all_side_items,
             'alldrinkitems': all_drink_items,
         })
+
+    else:
+        return HttpResponseRedirect(reverse('login:login'))
+
+
+def create_meal(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            mealName = request.POST.get("mealName", "")
+            description = request.POST.get("description", "")
+            sideID = request.POST.get("mealSide", "")
+            price = request.POST.get("price", "")
+
+            if "mealHasDrink" in request.POST:
+                hasDrink = True
+            else:
+                hasDrink = False
+
+            if mealName == "" or description == "" or price == "" or sideID == "":
+                return HttpResponseRedirect(reverse('menu:menu'))
+
+            meal = Meal()
+            meal.name = mealName
+            meal.description = description
+            meal.side = Side.objects.get(id=sideID)
+            meal.has_drink = hasDrink
+            meal.current_menu_item = True
+            meal.price = price
+
+            meal.save()
+
+            return HttpResponseRedirect(reverse('menu:menu'))
+
+        else:
+            return HttpResponseRedirect(reverse('login:index'))
+
+    else:
+        return HttpResponseRedirect(reverse('login:login'))
+
+
+def create_side(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            sideName = request.POST.get("sideName", "")
+            price = request.POST.get("sidePrice", "")
+
+            if sideName == "" or price == "":
+                return HttpResponseRedirect(reverse('menu:menu'))
+
+            side = Side()
+            side.name = sideName
+            side.price = price
+            side.current_menu_item = True
+
+            side.save()
+
+            return HttpResponseRedirect(reverse('menu:menu'))
+
+        else:
+            return HttpResponseRedirect(reverse('login:index'))
+
+    else:
+        return HttpResponseRedirect(reverse('login:login'))
+
+
+def create_drink(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            drinkName = request.POST.get("drinkName", "")
+            price = request.POST.get("drinkPrice", "")
+
+            if drinkName == "" or price == "":
+                return HttpResponseRedirect(reverse('menu:menu'))
+
+            drink = Drink()
+            drink.name = drinkName
+            drink.price = price
+            drink.current_menu_item = True
+
+            drink.save()
+
+            return HttpResponseRedirect(reverse('menu:menu'))
+
+        else:
+            return HttpResponseRedirect(reverse('menu:index'))
 
     else:
         return HttpResponseRedirect(reverse('login:login'))
